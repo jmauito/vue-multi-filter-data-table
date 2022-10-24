c8f6bb55bb2c434e234bd91b6bc0a45b5dd82085
Mauricio Zárate <mzaratep@ucacue.edu.ec>
2022-10-24 13:28:23 -0500

    Solution ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <data-table></data-table>
+    </v-main>
+  </v-app> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataTable from './components/DataTable';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    DataTable,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
